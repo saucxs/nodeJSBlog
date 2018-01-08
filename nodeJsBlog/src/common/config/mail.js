7645265de219e02c46e7b9a2eb52015e9eef2915
saucxs <15566151641@163.com>
2018-01-08 21:02:08 +0800

    update

---
+++ nodeJsBlog/src/common/config/mail.js
@@ -0,0 +1,15 @@
+'use strict';
+/**
+ * config
+ */
+export default {
+  //发送邮件配置
+  // host:"smtp.qq.com",
+  // port: 465,
+  // domains:'["qq.com"]',
+  // account:'262248861@qq.com',
+  // pass: "1234567",
+  // from:"262248861@qq.com",
+  // route_on: true,
+  // encoding: "utf-8"
+};
